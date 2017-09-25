a3bf44607aa362cede8c8c45762ff4e998af7775
Jim McDonald <Jim@mcdee.net>
2017-09-25 07:47:23 +0100

    Add loacl private network for testing

--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    private: {
+      host: "localhost",
+      port: 8555,
+      network_id: "*" // Match any network id
     }
   }
 };
