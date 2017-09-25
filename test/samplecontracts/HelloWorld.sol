b234aae133b8f5690690ef3d6eb6a87387c70a40
Jim McDonald <Jim@mcdee.net>
2017-09-25 07:47:10 +0100

    Add hello world test contract

---
+++ test/samplecontracts/HelloWorld.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.11;
+
+
+// Simple hello world contract
+contract HelloWorld {
+    uint256 HELLO_WORLD = 1;
+    function hello() public constant returns (uint256) {
+        return WORLD;
+    }
+}
