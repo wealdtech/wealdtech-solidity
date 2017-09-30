e00f0fdb5040035fe345a39f3b93ac27a1cccd4b
Jim McDonald <Jim@mcdee.net>
2017-09-30 10:47:44 +0100

    Add all optional elements of ERC20

--- contracts/token/IERC20.sol
+++ contracts/token/IERC20.sol
@@ -7,12 +7,17 @@ pragma solidity ^0.4.11;
  *        ERC20 is defined at https://github.com/ethereum/EIPs/issues/20
  */
 contract IERC20 {
-    function balanceOf(address who) constant returns (uint256);
-    function transfer(address to, uint256 value) returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    string public name;
+    string public symbol;
+    uint8 public decimals;
 
-    function allowance(address owner, address spender) constant returns (uint256);
-    function transferFrom(address from, address to, uint256 value) returns (bool);
-    function approve(address spender, uint256 value) returns (bool);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    function totalSupply() constant returns (uint256 totalSupply);
+    function balanceOf(address _owner) constant returns (uint256 balance);
+    function transfer(address _owner, uint256 _value) returns (bool success);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+    function transferFrom(address _owner, address _to, uint256 _value) returns (bool success);
+    function approve(address _spender, uint256 _value) returns (bool success);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
