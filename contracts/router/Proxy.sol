e3da8e1be43b278428d1f84c6ed78e17a015c5a5
Jim McDonald <Jim@mcdee.net>
2017-12-09 08:27:29 +0000

    Update proxy - WIP

--- contracts/router/Proxy.sol
+++ contracts/router/Proxy.sol
@@ -1,17 +1,23 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.18;
 
 
 contract Proxy {
-    function() public payable {
+    address public target;
+
+    function () public payable {
+        address _target = target;
         assembly {
-            switch delegatecall(gas, 0x0000, 0, calldatasize, 0, 0)
-            case 0 {
-                revert(0, 0)
-            }
-            default {
-                returndatacopy(0, 0, returndatasize)
-                return(0, returndatasize)
-            }
+          let _calldata := mload(0x40)
+          mstore(0x40, add(_calldata, calldatasize))
+          calldatacopy(_calldata, 0x0, calldatasize)
+          switch delegatecall(gas, _target, _calldata, calldatasize, 0, 0)
+              case 0 {
+                  revert(0, 0)
+              }
+              default {
+                  returndatacopy(0, 0, returndatasize)
+                  return(0, returndatasize)
+              }
         }
     }
 }
