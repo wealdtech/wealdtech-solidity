7a39404147093a0a75680fcc1097c357cafcead0
Jim McDonald <Jim@mcdee.net>
2018-06-20 00:02:11 +0100

    Updates

---
+++ contracts/tokensrecipient/Forwarder.sol
@@ -0,0 +1,71 @@
+pragma solidity ^0.4.18;
+
+import '../token/ERC777TokensRecipient.sol';
+import '../token/IERC777.sol';
+import 'eip820/contracts/ERC820Implementer.sol';
+
+
+/**
+ * @title Forwarder
+ *  
+ *        An ERC777 tokens recipient contract that forwards tokens to another
+ *        address.  Commonly used to forward received tokens to an aggregate
+ *        store, for example an exchange could forward deposited tokens directly
+ *        to cold storage.
+ *
+ *        Note that because this contract transfers token on behalf of the
+ *        recipient it requires operator privileges.
+ *
+ *        State of this contract: stable; development complete but the code is
+ *        unaudited. and may contain bugs and/or security holes. Use at your own
+ *        risk.
+ *  
+ * @author Jim McDonald
+ * @notice If you use this contract please consider donating some Ether or
+ *         some of your ERC-777 token to wsl.wealdtech.eth to support continued
+ *         development of these and future contracts
+ */
+contract Forwarder is ERC777TokensRecipient, ERC820Implementer {
+    // recipient=>target
+    mapping(address=>address) public forwardingAddresses;
+
+    // An event emitted when a forwarding address is set
+    event ForwardingAddressSet(address recipient, address target);
+    // An event emitted when a forwarding address is cleared
+    event ForwardingAddressCleared(address recipient);
+
+    function setForwarder(address target) public {
+        forwardingAddresses[msg.sender] = target;
+        emit ForwardingAddressSet(msg.sender, target);
+    }
+
+    function clearForwarder() public {
+        delete(forwardingAddresses[msg.sender]);
+        emit ForwardingAddressCleared(msg.sender);
+    }
+
+    function getForwarder(address target) public constant returns (address) {
+        return forwardingAddresses[target];
+    }
+
+    /**
+     * tokensReceived forwards the token if a forwarder is set.
+     */
+    function tokensReceived(address operator, address holder, address recipient, uint256 amount, bytes holderData, bytes operatorData) public {
+        (operator, holder, holderData, operatorData);
+        if (forwardingAddresses[recipient] != 0) {
+            IERC777 tokenContract = IERC777(msg.sender);
+            // Transfer the tokens - this throws if it fails
+            tokenContract.operatorSend(recipient, forwardingAddresses[recipient], amount, "", "");
+        }
+    }
+
+    function canImplementInterfaceForAddress(address addr, bytes32 interfaceHash) pure public returns(bytes32) {
+        (addr);
+        if (interfaceHash == keccak256("ERC777TokensRecipient")) {
+            return keccak256("ERC820_ACCEPT_MAGIC");
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ test/tokensrecipient/Forwarder.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const assertRevert = require('../helpers/assertRevert.js');
+
+const ERC777Token = artifacts.require('ERC777Token');
+const Forwarder = artifacts.require('Forwarder');
+const ERC820Registry = artifacts.require('ERC820Registry');
+
+contract('Forwarder', accounts => {
+    var erc777Instance;
+    var erc820Instance;
+    var instance;
+
+    let expectedBalances = [
+        web3.toBigNumber(0),
+        web3.toBigNumber(0),
+        web3.toBigNumber(0),
+        web3.toBigNumber(0),
+        web3.toBigNumber(0)
+    ];
+    const initialSupply = web3.toBigNumber('1000000000000000000000');
+    const granularity = web3.toBigNumber('10000000000000000');
+
+    // Helper to confirm that balances are as expected
+    async function confirmBalances() {
+        for (var i = 0; i < expectedBalances.length; i++) {
+            assert.equal((await erc777Instance.balanceOf(accounts[i])).toString(10), expectedBalances[i].toString(10), 'Balance of account ' + i + ' is incorrect');
+        }
+        // Also confirm total supply
+        assert.equal((await erc777Instance.totalSupply()).toString(), expectedBalances.reduce((a, b) => a.add(b), web3.toBigNumber('0')).toString(), 'Total supply is incorrect');
+    }
+
+    it('sets up', async function() {
+        erc820Instance = await ERC820Registry.at('0x991a1bcb077599290d7305493c9a630c20f8b798');
+        erc777Instance = await ERC777Token.new(1, 'Test token', 'TST', granularity, initialSupply, 0, {
+            from: accounts[0],
+            gas: 10000000
+        });
+        await erc777Instance.activate({
+            from: accounts[0]
+        });
+        expectedBalances[0] = initialSupply.mul(1);
+        await confirmBalances();
+    });
+
+    it('creates the recipient contract', async function() {
+        instance = await Forwarder.new({
+            from: accounts[0]
+        });
+    });
+
+    it('forwards tokens accordingly', async function() {
+        // Register the recipient
+        await erc820Instance.setInterfaceImplementer(accounts[1], web3.sha3("ERC777TokensRecipient"), instance.address, {
+            from: accounts[1]
+        });
+
+        // Set up forwarding from accounts[1] to accounts[4]
+        await instance.setForwarder(accounts[4], {
+            from: accounts[1]
+        });
+        assert.equal(await instance.getForwarder(accounts[1]), accounts[4]);
+
+        // Set up the recipient contract as an operator for accounts[1]
+        await erc777Instance.authorizeOperator(instance.address, {
+            from: accounts[1]
+        });
+
+        // Transfer 100*granularity tokens from accounts[0] to accounts[1]
+        await erc777Instance.send(accounts[1], granularity.mul(100), "", {
+            from: accounts[0]
+        });
+        expectedBalances[0] = expectedBalances[0].sub(granularity.mul(100));
+        expectedBalances[4] = expectedBalances[4].add(granularity.mul(100));
+        await confirmBalances();
+
+        // Unregister the operator
+        await erc777Instance.revokeOperator(instance.address, {
+            from: accounts[0]
+        });
+
+        // Unregister the recipient
+        await erc820Instance.setInterfaceImplementer(accounts[1], web3.sha3("ERC777TokensRecipient"), 0, {
+            from: accounts[1]
+        });
+    });
+});
