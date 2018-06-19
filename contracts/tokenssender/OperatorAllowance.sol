d498fc4d0e50bf7fbb701cbc19ad2f4c7ae950df
Jim McDonald <Jim@mcdee.net>
2018-06-19 23:34:31 +0100

    Tidy-ups

---
+++ contracts/tokenssender/OperatorAllowance.sol
@@ -0,0 +1,69 @@
+pragma solidity ^0.4.18;
+
+import '../math/SafeMath.sol';
+import '../token/ERC777TokensSender.sol';
+import 'eip820/contracts/ERC820Implementer.sol';
+
+
+/**
+ * @title OperatorAllowance
+ *
+ *        An ERC777 tokens sender contract that provides operators with an
+ *        allowance of tokens to send rather than complete control of all
+ *        tokens in the sender's account.
+ *
+ *        State of this contract: stable; development complete but the code is
+ *        unaudited. and may contain bugs and/or security holes. Use at your own
+ *        risk.
+ *
+ * @author Jim McDonald
+ * @notice If you use this contract please consider donating some Ether or
+ *         some of your ERC-777 token to wsl.wealdtech.eth to support continued
+ *         development of these and future contracts
+ */
+contract OperatorAllowance is ERC777TokensSender, ERC820Implementer {
+    using SafeMath for uint256;
+
+    // Mapping is holder=>operator=>token=>allowance
+    mapping(address=>mapping(address=>mapping(address=>uint256))) public allowances;
+
+    // An event emitted when an allowance is set
+    event AllowanceSet(address holder, address operator, address token, uint256 allowance);
+
+    /**
+     * setAllowance sets an allowance.
+     */
+    function setAllowance(address _operator, address _token, uint256 _currentAllowance, uint256 _newAllowance) public {
+        require(allowances[msg.sender][_operator][_token] == _currentAllowance);
+        allowances[msg.sender][_operator][_token] = _newAllowance;
+        emit AllowanceSet(msg.sender, _operator, _token, _newAllowance);
+    }
+
+    /**
+     * getAllowance gets an allowance.
+     */
+    function getAllowance(address _holder, address _operator, address _token) public constant returns (uint256) {
+        return allowances[_holder][_operator][_token];
+    }
+
+    function tokensToSend(address operator, address holder, address recipient, uint256 amount, bytes holderData, bytes operatorData) public {
+        (recipient, holderData, operatorData);
+
+        if (operator == holder) {
+            // This is a user send not an operator send; ignore
+            return;
+        }
+
+        require (allowances[holder][operator][msg.sender] >= amount);
+        allowances[holder][operator][msg.sender] = allowances[holder][operator][msg.sender].sub(amount);
+    }
+
+    function canImplementInterfaceForAddress(address addr, bytes32 interfaceHash) pure public returns(bytes32) {
+        (addr);
+        if (interfaceHash == keccak256("ERC777TokensSender")) {
+            return keccak256("ERC820_ACCEPT_MAGIC");
+        } else {
+            return 0;
+        }   
+    }
+}
---
+++ contracts/tokenssender/SupplementWitholdingAccount.sol
@@ -0,0 +1,98 @@
+pragma solidity ^0.4.18;
+
+import '../math/SafeMath.sol';
+import '../token/ERC777TokensSender.sol';
+import '../token/IERC777.sol';
+import 'eip820/contracts/ERC820Implementer.sol';
+
+
+/**
+ * @title SupplementWitholdingAccount
+ *
+ *        An ERC777 tokens sender contract that supplements a specified account
+ *        with a percentage of tokens transferred.  Commony used to divert funds
+ *        to a tax witholding account to avoid over-spending.
+ *
+ *        For example, if the percentage is set to 15 and 100 tokens are to be
+ *        sent then this will send an additional 15 tokens from the sender to
+ *        the holding account.  If sufficient tokens are not available then this
+ *        will revert.
+ *
+ *        Note that because this contract transfers token on behalf of the
+ *        sender it requires operator privileges.
+ *
+ *        State of this contract: stable; development complete but the code is
+ *        unaudited. and may contain bugs and/or security holes. Use at your own
+ *        risk.
+ *
+ * @author Jim McDonald
+ * @notice If you use this contract please consider donating some Ether or
+ *         some of your ERC-777 token to wsl.wealdtech.eth to support continued
+ *         development of these and future contracts
+ */
+contract SupplementWitholdingAccount is ERC777TokensSender, ERC820Implementer {
+    using SafeMath for uint256;
+
+    // The account to send the tokens
+    mapping(address=>address) public accounts;
+    // The percentage of tokens to send,  in 1/10000s of a percentage
+    mapping(address=>uint16) public percentages;
+
+    // An event emitted when a supplement is set
+    event SupplementSet(address holder, address target, uint16 percentage);
+    // An event emitted when a supplement is removed
+    event SupplementRemoved(address holder);
+
+    /**
+     * setSuplement sets an account and percentage to which to send tokens
+     * @param _target the address to which to send tokens
+     * @param _percentage the percentage of additional tokens ot send,
+     *        in 1/10000s of a percentage
+     */
+    function setSupplement(address _target, uint16 _percentage) public {
+        require(_target != 0);
+        accounts[msg.sender] = _target;
+        percentages[msg.sender] = _percentage;
+        emit SupplementSet(msg.sender, _target, _percentage);
+    }
+
+    /**
+     * removeSupplement removes a supplement
+     */
+    function removeSupplement() public {
+        accounts[msg.sender] = 0;
+        percentages[msg.sender] = 0;
+        emit SupplementRemoved(msg.sender);
+    }
+
+    function tokensToSend(address operator, address holder, address recipient, uint256 value, bytes holderData, bytes operatorData) public {
+        (operator);
+
+        require(accounts[holder] != 0);
+
+        // Ignore tokens already being sent to the target account
+        if (recipient == accounts[holder]) {
+            return;
+        }
+
+        IERC777 tokenContract = IERC777(msg.sender);
+        // Calculate the additional tokens to send
+        uint256 supplement = value.mul(uint256(percentages[holder])).div(uint256(10000));
+        // Round up in the case of the value being an odd granularity
+        uint256 granularity = tokenContract.granularity();
+        if (supplement % granularity != 0) {
+            supplement = (supplement.div(granularity)+1).mul(granularity);
+        }
+        // Transfer the tokens - this throws if it fails
+        tokenContract.operatorSend(holder, accounts[holder], supplement, holderData, operatorData);
+    }
+
+    function canImplementInterfaceForAddress(address addr, bytes32 interfaceHash) pure public returns(bytes32) {
+        (addr);
+        if (interfaceHash == keccak256("ERC777TokensSender")) {
+            return keccak256("ERC820_ACCEPT_MAGIC");
+        } else {
+            return 0;
+        }   
+    }
+}
