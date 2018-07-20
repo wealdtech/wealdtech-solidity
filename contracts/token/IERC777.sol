730e31eea0d63b1d685ddc84b41e51caff73708a
Jim McDonald <Jim@mcdee.net>
2018-07-20 10:11:27 +0100

    Add burn functions

--- contracts/token/IERC777.sol
+++ contracts/token/IERC777.sol
@@ -14,11 +14,13 @@ contract IERC777 {
     function balanceOf(address owner) public constant returns (uint256);
 
     function send(address to, uint256 amount, bytes userData) public;
+    function burn(uint256 amount, bytes userData) public;
 
     function authorizeOperator(address operator) public;
     function revokeOperator(address operator) public;
     function isOperatorFor(address operator, address tokenHolder) public constant returns (bool);
     function operatorSend(address from, address to, uint256 amount, bytes userData, bytes operatorData) public;
+    function operatorBurn(address from, uint256 amount, bytes userData, bytes operatorData) public;
 
     event Sent(address indexed operator, address indexed from, address indexed to, uint256 amount, bytes userData, bytes operatorData);
     event Minted(address indexed operator, address indexed to, uint256 amount, bytes userData);
